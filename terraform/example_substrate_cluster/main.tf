1ce08979aeb2f13c4288a243c0c9b4bf1f9a1988
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-03-16 18:51:15 -0300

    Rename example directory

--- terraform/example_substrate_cluster/main.tf
+++ terraform/example_substrate_cluster/main.tf
@@ -2,7 +2,6 @@ module "substrate_cluster_1" {
   source = "../modules/aws_ec2_cluster"
 
   cluster_name = "substrate_test_cluster"
-  # instance_type = "t2.micro"
   ssh_key_name = "klaus"
   node_count = 3
 }
